d549a224cc3d66eb38b243188464704bb77016c2
Patryk Dobrzyński <patryk.dobrzynski@o2.pl>
2021-11-28 21:19:02 +0100

    Init commit

---
+++ src/app/tutorial/tutorial.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-tutorial',
+  templateUrl: './tutorial.component.html',
+  styleUrls: ['./tutorial.component.scss'],
+})
+export class TutorialComponent implements OnInit {
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  public navigateToMenu() {
+    this.router.navigate(['/menu']);
+  }
+
+}
