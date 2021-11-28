217134eda8fce7a8edb5037fefabeeda1eee0e5d
Patryk Dobrzyński <patryk.dobrzynski@o2.pl>
2021-11-28 21:19:02 +0100

    Init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {TutorialComponent} from "./tutorial/tutorial.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'menu',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    component: TutorialComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   }
 ];
 @NgModule({
---
+++ src/app/tutorial/tutorial.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {TutorialComponent} from "./tutorial.component";
+
+
+
+@NgModule({
+  declarations: [
+    TutorialComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TutorialModule { }
