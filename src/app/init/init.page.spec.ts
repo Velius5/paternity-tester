4a21b8304b7bc7f705b12851a431ba5e21b9b56f
Patryk Dobrzyński <patryk.dobrzynski@o2.pl>
2022-01-10 22:04:21 +0100

    Added init page

--- src/app/init/init.page.spec.ts
+++ src/app/init/init.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { ExploreContainerComponent } from './explore-container.component';
+import { InitPage } from './init.page';
 
-describe('ExploreContainerComponent', () => {
-  let component: ExploreContainerComponent;
-  let fixture: ComponentFixture<ExploreContainerComponent>;
+describe('InitPage', () => {
+  let component: InitPage;
+  let fixture: ComponentFixture<InitPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExploreContainerComponent ],
+      declarations: [ InitPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ExploreContainerComponent);
+    fixture = TestBed.createComponent(InitPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
